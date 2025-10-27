b3ce590b83d40891c065b88c594645ffa673dd07
Shmelev Valentin <shmelevvl@mail.ru>
2025-10-27 19:33:17 +0700

    gen actor spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
 import { ReviewModule } from './review/review.module';
+import { ActorModule } from './actor/actor.module';
 
 @Module({
-  imports: [MovieModule, ReviewModule],
+  imports: [MovieModule, ReviewModule, ActorModule],
   controllers: [AppController],
   providers: [AppService],
 })
